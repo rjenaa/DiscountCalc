97bb94ab7e23231480ac6201145827b77a1b2f90
Ray <54691205+rjenaa@users.noreply.github.com>
2020-10-15 22:13:14 -0400

    af

--- server.js
+++ server.js
@@ -1,15 +1,16 @@
-
+//Install express server
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
-app.use(express.static('./smallCalc/price-calc-angular'));
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/<price-calc-angular>'));
 
-app.get('/*', function (req, res) {
-  res.sendFile('index.html', { root: 'smallCalc/price-calc-angular' }
-  );
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/<price-calc-angular>/index.html'));
 });
 
+// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
-
-console.log(`Running on port ${process.env.PORT || 8080}`)
